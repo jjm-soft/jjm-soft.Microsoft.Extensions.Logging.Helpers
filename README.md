c561fea8d200a3ed3c4df482bcbcb939ba3aaec7
Jonas Merkle <traineeit@falch.com>
2023-02-01 18:22:17 +0100

    Function logging added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Microsoft.Extensions.Logging.Helpers
+
 A collection of helper functions for the Microsoft logging tool.
---
+++ src/Microsoft.Extensions.Logging.Helpers/FunctionLogging/FunctionLogging.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace Microsoft.Extensions.Logging.Helpers
+{
+    /// <summary>
+    /// Static class for all function logging helper extensions.
+    /// </summary>
+    public static  class FunctionLogging
+    {
+
+        /// <summary>
+        /// Log a function call.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="classType">The <see cref="Type"/> of the calling class.</param>
+        /// <param name="methodType">The <see cref="MethodBase"/> of the calling method/function.</param>
+        /// <param name="level">The logging level. Default is <see cref="LogLevel.Debug"/>.</param>
+        public static void LogFctCall(this ILogger logger, Type? classType, MethodBase? methodType, LogLevel level = LogLevel.Debug)
+        {
+            logger.Log(level, "Function called: {ClassName} -> {FctName}", classType?.Name, methodType?.Name);
+        }
+
+        /// <summary>
+        /// Log an exception within a function call.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="exc">The exception.</param>
+        /// <param name="classType">The <see cref="Type"/> of the calling class.</param>
+        /// <param name="methodType">The <see cref="MethodBase"/> of the calling method/function.</param>
+        /// <param name="msg">The message of the exception.</param>
+        /// <param name="level">The logging level. Default is <see cref="LogLevel.Error"/>.</param>
+        public static void LogExcInFctCall(this ILogger logger, Exception exc, Type? classType,
+            MethodBase? methodType, string? msg = null, LogLevel level = LogLevel.Error)
+        {
+            if (string.IsNullOrEmpty(msg))
+            {
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}", classType?.Name, methodType?.Name);
+            }
+            else
+            {
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}\n" + msg, classType?.Name, methodType?.Name);
+            }
+        }
+    }
+}

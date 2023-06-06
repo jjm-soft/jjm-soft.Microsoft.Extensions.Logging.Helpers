1f47f3e1f664676bb8532b76d01a1059c04d7daf
jonas-merkle <jonas@jjm.one>
2023-06-06 01:02:17 +0000

    Deploying to gh-pages from @ jjm-one/jjm.one.Microsoft.Extensions.Logging.Helpers@7a64700b06fffb87dfe8deee77782fec25be6410 🚀

---
+++ develop/doc/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['logexcinfctcall_21',['LogExcInFctCall',['../classjjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers_1_1FunctionLogging.html#a483770d099b46f7bdea5a29ba9564bce',1,'jjm::one::Microsoft::Extensions::Logging::Helpers::FunctionLogging']]],
+  ['logfctcall_22',['LogFctCall',['../classjjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers_1_1FunctionLogging.html#a912b3ae029d9ff1490ac63f26ca17fec',1,'jjm::one::Microsoft::Extensions::Logging::Helpers::FunctionLogging']]]
+];

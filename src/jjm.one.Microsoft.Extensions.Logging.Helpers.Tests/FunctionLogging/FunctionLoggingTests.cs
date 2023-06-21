4e0c6d2296e9c08abf1edf6eef1b8d3a02cb8984
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-22 00:58:37 +0200

    removes classType & methodType

---
+++ src/jjm.one.Microsoft.Extensions.Logging.Helpers.Tests/FunctionLogging/FunctionLoggingTests.cs
@@ -0,0 +1,6 @@
+namespace jjm.one.Microsoft.Extensions.Logging.Helpers.Tests.FunctionLogging;
+
+public class FunctionLoggingTests
+{
+    
+}
--- src/jjm.one.Microsoft.Extensions.Logging.Helpers/FunctionLogging/FunctionLogging.cs
+++ src/jjm.one.Microsoft.Extensions.Logging.Helpers/FunctionLogging/FunctionLogging.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace jjm.one.Microsoft.Extensions.Logging.Helpers
@@ -11,24 +12,63 @@ namespace jjm.one.Microsoft.Extensions.Logging.Helpers
     {
 
         /// <summary>
-        /// Log a function call.
+        /// Log a function/method call.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="level">The logging level. Default is <see cref="LogLevel.Debug"/>.</param>
+        public static void LogFctCall(this ILogger logger, LogLevel level = LogLevel.Debug)
+        {
+            var method = new StackTrace().GetFrame(1)?.GetMethod();
+
+            logger.Log(level, "Function called: {ClassName} -> {FctName}",
+                method?.DeclaringType?.Name, method?.Name);
+        }
+        
+        /// <summary>
+        /// Log a function/method call.
         /// </summary>
         /// <param name="logger">The logger instance.</param>
         /// <param name="classType">The <see cref="Type"/> of the calling class.</param>
-        /// <param name="methodType">The <see cref="MethodBase"/> of the calling method/function.</param>
+        /// <param name="methodType">The <see cref="MethodBase"/> of the calling function/method.</param>
         /// <param name="level">The logging level. Default is <see cref="LogLevel.Debug"/>.</param>
-        public static void LogFctCall(this ILogger logger, Type? classType, MethodBase? methodType, LogLevel level = LogLevel.Debug)
+        public static void LogFctCall(this ILogger logger, Type? classType, MethodBase? methodType, 
+            LogLevel level = LogLevel.Debug)
         {
-            logger.Log(level, "Function called: {ClassName} -> {FctName}", classType?.Name, methodType?.Name);
+            logger.Log(level, "Function called: {ClassName} -> {FctName}", 
+                classType?.Name, methodType?.Name);
+        }
+        
+        /// <summary>
+        /// Log an exception within a function/method call.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="exc">The exception.</param>
+        /// <param name="msg">The message of the exception.</param>
+        /// <param name="level">The logging level. Default is <see cref="LogLevel.Error"/>.</param>
+        public static void LogExcInFctCall(this ILogger logger, Exception exc, string? msg = null, 
+            LogLevel level = LogLevel.Error)
+        {
+            var method = new StackTrace().GetFrame(1)?.GetMethod();
+            
+            if (string.IsNullOrEmpty(msg))
+            {
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}",
+                    method?.DeclaringType?.Name, method?.Name);
+            }
+            else
+            {
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}\n" + msg,
+                    method?.DeclaringType?.Name, method?.Name);
+            }
         }
 
         /// <summary>
-        /// Log an exception within a function call.
+        /// Log an exception within a function/method call.
         /// </summary>
         /// <param name="logger">The logger instance.</param>
         /// <param name="exc">The exception.</param>
         /// <param name="classType">The <see cref="Type"/> of the calling class.</param>
-        /// <param name="methodType">The <see cref="MethodBase"/> of the calling method/function.</param>
+        /// <param name="methodType">The <see cref="MethodBase"/> of the calling function/method.</param>
         /// <param name="msg">The message of the exception.</param>
         /// <param name="level">The logging level. Default is <see cref="LogLevel.Error"/>.</param>
         public static void LogExcInFctCall(this ILogger logger, Exception exc, Type? classType,
@@ -36,11 +76,13 @@ namespace jjm.one.Microsoft.Extensions.Logging.Helpers
         {
             if (string.IsNullOrEmpty(msg))
             {
-                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}", classType?.Name, methodType?.Name);
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}", 
+                    classType?.Name, methodType?.Name);
             }
             else
             {
-                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}\n" + msg, classType?.Name, methodType?.Name);
+                logger.Log(level, exc, "Exception thrown in: {ClassName} -> {FctName}\n" + msg, 
+                    classType?.Name, methodType?.Name);
             }
         }
     }

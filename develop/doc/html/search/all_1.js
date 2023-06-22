85989c9e3a0e27c758cebc1a3a736e49db270ba2
jonas-merkle <jonas@jjm.one>
2023-06-22 09:51:18 +0000

    Deploying to gh-pages from @ jjm-one/jjm.one.Microsoft.Extensions.Logging.Helpers@85f2579f9277c42cb49b708e807a9bef4db63b82 🚀

--- develop/doc/html/search/all_1.js
+++ develop/doc/html/search/all_1.js
@@ -1,10 +1,4 @@
 var searchData=
 [
-  ['extensions_2',['Extensions',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions.html',1,'jjm::one::Microsoft']]],
-  ['helpers_3',['Helpers',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers.html',1,'jjm::one::Microsoft::Extensions::Logging']]],
-  ['jjm_4',['jjm',['../namespacejjm.html',1,'']]],
-  ['jjm_2eone_2emicrosoft_2eextensions_2elogging_2ehelpers_5',['jjm.one.Microsoft.Extensions.Logging.Helpers',['../index.html',1,'']]],
-  ['logging_6',['Logging',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging.html',1,'jjm::one::Microsoft::Extensions']]],
-  ['microsoft_7',['Microsoft',['../namespacejjm_1_1one_1_1Microsoft.html',1,'jjm::one']]],
-  ['one_8',['one',['../namespacejjm_1_1one.html',1,'jjm']]]
+  ['jjm_2eone_2emicrosoft_2eextensions_2elogging_2ehelpers_2',['jjm.one.Microsoft.Extensions.Logging.Helpers',['../index.html',1,'']]]
 ];

c6fea0e1365e717153002a694765c381301fcd55
jonas-merkle <jonas@jjm.one>
2023-07-04 00:16:31 +0000

    Deploying to gh-pages from @ jjm-one/jjm.one.Microsoft.Extensions.Logging.Helpers@7247a04f092048fa885fb28e2b138b3b0587e952 🚀

--- main/doc/html/search/all_2.js
+++ main/doc/html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['logexcinfctcall_9',['LogExcInFctCall',['../classjjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers_1_1FunctionLogging.html#a483770d099b46f7bdea5a29ba9564bce',1,'jjm::one::Microsoft::Extensions::Logging::Helpers::FunctionLogging']]],
-  ['logfctcall_10',['LogFctCall',['../classjjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers_1_1FunctionLogging.html#a912b3ae029d9ff1490ac63f26ca17fec',1,'jjm::one::Microsoft::Extensions::Logging::Helpers::FunctionLogging']]]
+  ['logexcinfctcall_3',['LogExcInFctCall',['../classFunctionLogging.html#a21a38b7dfc5da8b89afc6e4c7fd1e23e',1,'FunctionLogging.LogExcInFctCall(this ILogger logger, Exception exc, string? msg=null, LogLevel level=LogLevel.Error)'],['../classFunctionLogging.html#a700b1c9bf0878d8eade0f010fe07c138',1,'FunctionLogging.LogExcInFctCall(this ILogger logger, Exception exc, Type? classType, MethodBase? methodType, string? msg=null, LogLevel level=LogLevel.Error)']]],
+  ['logfctcall_4',['LogFctCall',['../classFunctionLogging.html#aeb9540dc6debbecb49edc06459fdb230',1,'FunctionLogging.LogFctCall(this ILogger logger, LogLevel level=LogLevel.Debug)'],['../classFunctionLogging.html#a59f48c0d75f98a49ea3745adc41d35c5',1,'FunctionLogging.LogFctCall(this ILogger logger, Type? classType, MethodBase? methodType, LogLevel level=LogLevel.Debug)']]]
 ];
--- main/doc/html/search/namespaces_0.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['extensions_13',['Extensions',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions.html',1,'jjm::one::Microsoft']]],
-  ['helpers_14',['Helpers',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging_1_1Helpers.html',1,'jjm::one::Microsoft::Extensions::Logging']]],
-  ['jjm_15',['jjm',['../namespacejjm.html',1,'']]],
-  ['logging_16',['Logging',['../namespacejjm_1_1one_1_1Microsoft_1_1Extensions_1_1Logging.html',1,'jjm::one::Microsoft::Extensions']]],
-  ['microsoft_17',['Microsoft',['../namespacejjm_1_1one_1_1Microsoft.html',1,'jjm::one']]],
-  ['one_18',['one',['../namespacejjm_1_1one.html',1,'jjm']]]
-];
